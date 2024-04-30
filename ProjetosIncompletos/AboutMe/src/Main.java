71b1a4d3215f4cedbedf68e56323b83685b1f39a
Gabriel_GDSF <Gabriel_GDSF7@outlook.com>
2024-04-30 17:05:03 -0300

    Update do repositório.

---
+++ ProjetosIncompletos/AboutMe/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 19;
+        String nome = "Gabriel", aprendizado = "junior developer";
+        double altura = 1.76;
+        System.out.println("Hi, my name is "+ nome + " I'm "+ idade + " years old, I'm " + altura + " tall and I'm " + aprendizado + " in back-end");
+    }
+}
