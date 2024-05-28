2374fdd87291ddbbe5089fd79501893219c4d15a
Gabriel_GDSF <Gabriel_GDSF7@outlook.com>
2024-05-28 16:59:03 -0300

    update do repositório

---
+++ Desafios/Desafio10/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+abstract class Cofre {
+    protected String tipo;
+    protected String metodoAbertura;
+
+    public Cofre(String tipo, String metodoAbertura) {
+        this.tipo = tipo;
+        this.metodoAbertura = metodoAbertura;
+    }
+
+    public void imprimirInformacoes() {
+        System.out.println("Tipo: " + this.tipo);
+        System.out.println("Metodo de abertura: " + this.metodoAbertura);
+    }
+}
+
+class CofreDigital extends Cofre {
+
+    private int senha;
+
+    public CofreDigital() {
+        super("Cofre Digital", "Senha");
+        ;
+    }
+}
+
+class CofreFisico extends Cofre {
+
+    public CofreFisico() {
+        super("Cofre Fisico", "Chave");
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Lê o tipo de cofre (primeira linha da entrada)
+        Scanner scanner = new Scanner(System.in);
+        String tipoCofre = scanner.nextLine();
+
+        if (tipoCofre.equalsIgnoreCase("digital")) {
+            String metodoAbertura = "Senha";
+            CofreDigital cofre = new CofreDigital();
+            cofre.imprimirInformacoes();
+            int senha = scanner.nextInt();
+            int confirmarSenha = scanner.nextInt();
+            if (confirmarSenha == senha){
+                System.out.println("Cofre aberto!");
+            }
+            else{
+                System.out.println("Senha incorreta!");
+            }
+        }
+        else{
+            CofreFisico cofreFisico = new CofreFisico();
+            cofreFisico.imprimirInformacoes();
+        }
+
+
+        }
+    }
